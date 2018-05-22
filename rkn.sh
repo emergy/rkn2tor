1f977c82aa1fe214664b18d96cba4248a785820c
root <root@debian.localhost>
2018-05-22 17:57:11 +0300

    store list file in /tmp

--- rkn.sh
+++ rkn.sh
@@ -12,10 +12,10 @@ ipset create rkn hash:ip hashsize 16777216 maxelem 16777216
 iptables -t nat -I PREROUTING 1 -m set --match-set rkn src -p tcp --syn -j REDIRECT --to-ports $PROXY_PORT
 iptables -t nat -I PREROUTING 1 -m set --match-set rkn dst -p tcp --syn -j REDIRECT --to-ports $PROXY_PORT
 
-curl -s https://api.reserve-rbl.ru/api/v2/ips/csv -o/tmp/rkn-list.tmp
-grep -Ev '([0-9]{1,3}\.){3}[0-9]{1,3}' /tmp/rkn-list.tmp >/dev/null || mv /tmp/rkn-list.tmp /tmp/rkn-list.txt
+curl -s https://api.reserve-rbl.ru/api/v2/ips/csv -o$TMP_FILE
+grep -Ev '([0-9]{1,3}\.){3}[0-9]{1,3}' $TMP_FILE >/dev/null || mv $TMP_FILE $LIST_FILE
 
-for i in `cat /tmp/rkn-list.txt`; do
+for i in `cat $LIST_FILE`; do
     ipset -A rkn $i
 done
 
